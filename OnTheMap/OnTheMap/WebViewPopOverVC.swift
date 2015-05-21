0244b82976f032b3b76357f73510bb1924ba7ec4
KT <kt@KTs-iMac.local>
2015-05-21 09:36:30 -0600

    Improved browsing vc. added back, forward, stop and refresh buttons

--- OnTheMap/OnTheMap/WebViewPopOverVC.swift
+++ OnTheMap/OnTheMap/WebViewPopOverVC.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
-class WebViewPopOverVC: UIViewController, UITextFieldDelegate {
+class WebViewPopOverVC: UIViewController, UITextFieldDelegate, UIWebViewDelegate {
 
     @IBOutlet weak var webView: UIWebView!
     @IBOutlet weak var urlField: UITextField!
+    
+    @IBOutlet weak var backButton: UIBarButtonItem!
+    @IBOutlet weak var forwardButton: UIBarButtonItem!
     
     var delegate: PostLocationPopOverVC? = nil
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        backButton.enabled = false
+        forwardButton.enabled = false
+        
+        webView.delegate = self
         
         let url = NSURL(string:"http://www.google.com")
         let request = NSURLRequest(URL:url!)
@@ -31,12 +39,47 @@ class WebViewPopOverVC: UIViewController, UITextFieldDelegate {
         return false
     }
     
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+
+        println("starting load with: ")
+        println(request)
+        
+        return true
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        println("finish load")
+        backButton.enabled = webView.canGoBack
+        forwardButton.enabled = webView.canGoForward
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    
+    @IBAction func backButtonTouch(sender: AnyObject) {
+        webView.goBack()
+    }
 
+    @IBAction func forwardButtonTouch(sender: AnyObject) {
+        webView.goForward()
+    }
+
+    @IBAction func stopButtonTouch(sender: AnyObject) {
+        webView.stopLoading()
+    }
+    
+    @IBAction func refreshButtonTouch(sender: AnyObject) {
+        let request = NSURLRequest(URL:webView.request!.URL!)
+        webView.loadRequest(request)
+    }
+    
     @IBAction func useLink(sender: AnyObject) {
         
         // println(webView.request!.URL!.absoluteString!)
