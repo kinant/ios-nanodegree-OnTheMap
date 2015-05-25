86b2621fb96380cd6fb13f658daa9525a6037253
Kinan T <kinan.t@gmail.com>
2015-05-25 15:49:14 -0600

    will attempt to add facebook login

---
+++ OnTheMap/Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  OnTheMap
+//
+//  Created by KT on 5/25/15.
+//  Copyright (c) 2015 Kinan Turjman. All rights reserved.
+//
+
+#ifndef OnTheMap_Bridging_Header_h
+#define OnTheMap_Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif
