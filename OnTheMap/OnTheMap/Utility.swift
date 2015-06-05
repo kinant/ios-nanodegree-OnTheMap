f74fb5ed9d886d9e68a4c75b68d76f02ec6096b6
Kinan T <kinan.t@gmail.com>
2015-06-04 21:49:52 -0600

    Commented ultility.

--- OnTheMap/OnTheMap/Utility.swift
+++ OnTheMap/OnTheMap/Utility.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+// function to delay app
+// from: http://stackoverflow.com/questions/24034544/dispatch-after-gcd-in-swift
 func delay(delay:Double, closure:()->()) {
     dispatch_after(
         dispatch_time(
@@ -17,12 +19,14 @@ func delay(delay:Double, closure:()->()) {
         dispatch_get_main_queue(), closure)
 }
 
+// declare the app colors to be used
 struct AppColors {
     static let MainBlueColor = UIColor(red: 118/255, green: 205/255, blue: 252/255, alpha: 1.0)
     static let LightBlueColor = UIColor(red: 113/255, green: 203/255, blue: 255/255, alpha: 1.0)
     static let DarkBlueColor = UIColor(red: 94/255, green: 166/255, blue: 207/255, alpha: 1.0)
 }
 
+// function for activating/disactivating the status bar activity indicator
 func activityIndicatorEnabled(enabled: Bool){
     UIApplication.sharedApplication().networkActivityIndicatorVisible = enabled
 }
